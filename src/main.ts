25773402e65ea3a8240fad9afb3bd660a9fd8b59
Shokir <shokiribnshuxrat2009@gmail.com>
2023-08-09 10:37:38 +0500

    commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  try {
+    const app = await NestFactory.create(AppModule);
+    const PORT = process.env.PORT || 3030;
+    app.setGlobalPrefix('/api');
+    await app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
